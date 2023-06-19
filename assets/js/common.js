eeec1de984c2d0ce7734ea5b1e172f21c9662da7
gdfqrqr <eunmikim0114@gmail.com>
2023-06-20 00:07:22 +0900

    230619_02

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,42 +1,62 @@
 $(function(){
+    gsap.registerPlugin(ScrollTrigger);
+    // ScrollTrigger.saveStyles(".mobile, .desktop");
+    ScrollTrigger.matchMedia({
+        "(min-width: 1023px)": function() {
+                //about
+            let sections = gsap.utils.toArray(".about-item");
 
-    //visual
+            gsap.to(sections, {
+            xPercent: -100 * (sections.length - 1),
+            ease: "none",
+            scrollTrigger: {
+                trigger: ".sc-about",
+                pin: true,
+                scrub: 1,
+                snap: 1 / (sections.length - 1),
+                // end: () => "+=" + document.querySelector(".sc-about .inner").offsetWidth
+            }
+            });
+        },
+        "all": function() {
+                //visual
+            gsap.to('.icon',0.7, {opacity:0})
 
+            var word = new TimelineMax()
+            .addLabel('b')
+            .to('.word',0.7, {visibility:'visible'},'b')
+            .to('.word',0.7, {opacity:1},'b')
 
+            .addLabel('a')
+            .to('.word',0.7, {opacity:0},'a')
+            .to('.icon',0.7, {opacity:1},'a')
 
+            .repeat(-1);
+
+            gsap.to('.icon2',0.9, {opacity:0})
+
+            var word2 = new TimelineMax()
+            .addLabel('b')
+            .to('.word2',0.7, {visibility:'visible',delay:0.5},'b')
+            .to('.word2',0.7, {opacity:1,delay:0.5},'b')
+
+            .addLabel('a')
+            .to('.word2',0.7, {opacity:0,delay:0.5},'a')
+            .to('.icon2',0.7, {opacity:1,delay:0.5},'a')
+
+            .repeat(-1);
+
+            var circle = new TimelineMax()
+
+            .to('.circle',0.5, {width:'20vw',delay:1})
+            .to('.circle',0.5, {width:'6.5vw',delay:2})
+            .repeat(-1)
+
+        }
+
+    })
     
-    gsap.to('.icon',0.7, {opacity:0})
-
-    var word = new TimelineMax()
-    .addLabel('b')
-    .to('.word',0.7, {visibility:'visible'},'b')
-    .to('.word',0.7, {opacity:1},'b')
-
-    .addLabel('a')
-    .to('.word',0.7, {opacity:0},'a')
-    .to('.icon',0.7, {opacity:1},'a')
-
-    .repeat(-1);
-
-    gsap.to('.icon2',0.9, {opacity:0})
-
-    var word2 = new TimelineMax()
-    .addLabel('b')
-    .to('.word2',0.9, {visibility:'visible'},'b')
-    .to('.word2',0.9, {opacity:1},'b')
-
-    .addLabel('a')
-    .to('.word2',0.9, {opacity:0},'a')
-    .to('.icon2',0.9, {opacity:1},'a')
-
-    .repeat(-1);
-
-    var circle = new TimelineMax()
-
-    .to('.circle',0.5, {width:'20vw',delay:1})
-    .to('.circle',0.5, {width:'6.5vw',delay:2})
-    .repeat(-1)
-
+  
 
 
 
@@ -51,21 +71,7 @@ $(function(){
         $(this).removeClass('active');
     })
 
-    //about
-    gsap.registerPlugin(ScrollTrigger);
-    let sections = gsap.utils.toArray(".about-item");
 
-    gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1),
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".sc-about",
-        pin: true,
-        scrub: 1,
-        snap: 1 / (sections.length - 1),
-        // end: () => "+=" + document.querySelector(".sc-about .inner").offsetWidth
-    }
-    });
 
     //footer
     $('.link-mail').hover(function(){
